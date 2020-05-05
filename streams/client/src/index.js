d27fa00545abfea41ecf487b0613354c09111176
Alexandre Costa <alexandre.costa@farfetch.com>
2020-05-05 15:32:14 +0100

    feat(stream): Add redux devtools

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App'
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import reducers from './reducers'
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;    // for chrome devtools extension!
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.querySelector('#root'))
